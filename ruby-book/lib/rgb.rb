ccc7e62fece7fd9d2c3cb06251f2686d4ab7c7c8
MyPoZi <kxx555u.u@gmail.com>
2019-07-06 20:50:49 +0900

    [add] rgb変換機能をTDD

---
+++ ruby-book/lib/rgb.rb
@@ -0,0 +1,23 @@
+def to_hex(r, g, b)
+  # '#' +
+  #     r.to_s(16).rjust(2, '0') +
+  #     g.to_s(16).rjust(2, '0') +
+  #     b.to_s(16).rjust(2, '0')
+  #
+  # hex = '#'
+  # [r, g, b].each {|n| hex += n.to_s(16).rjust(2, '0')}
+  # hex
+  #
+  [r, g, b].inject('#') do |hex, n|
+    hex + n.to_s(16).rjust(2, '0')
+  end
+end
+
+def to_ints(hex)
+  # r, g, b = hex[1..2], hex[3..4], hex[5..6]
+  # [r, g, b].map do |s|
+  #   s.hex
+  # end
+  #
+  hex.scan(/\w\w/).map(&:hex)
+end
